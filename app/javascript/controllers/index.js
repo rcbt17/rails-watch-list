875bb726a2cd1be965914968a372894f52d0ab66
Robin Ciubotaru <robinciubotaru17@gmail.com>
2023-05-29 10:05:32 +0200

    ss

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import SearchController from "./search_controller"
 application.register("search", SearchController)
+
+import StarRatingController from "./star_rating_controller"
+application.register("star-rating", StarRatingController)
